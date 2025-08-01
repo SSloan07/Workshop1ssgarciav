e84ac634fb7d22dc920b6a198547d299d0f12e7a
SimónSloan <ssgarciav@eafit.com>
2025-07-31 20:16:24 -0500

    Creación de buscador basico con Html dentro del codigo

--- movie/views.py
+++ movie/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from .models import Movie
 # Create your views here.
 def home(request): 
-    return render(request,'home.html', {'name':'Simón Sloan García Villa :)'})
+    # return render(request,'home.html', {'name':'Simón Sloan García Villa :)'})
+    searchTerm = request.GET.get('searchMovie')
+    movies = Movie.objects.all()
+    return render(request, 'home.html', {'searchTerm':searchTerm, 'movies': movies})
+
 def about(request): 
     return render(request,'about.hmtl')
 
